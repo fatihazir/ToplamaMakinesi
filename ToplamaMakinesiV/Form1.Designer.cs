d87fc4b00341f1241a05576a159e76a184db037f
Fatih <fatih.hazir@bakircay.edu.tr>
2019-09-22 10:44:02 +0300

    Proje dosyası ekle.

---
+++ ToplamaMakinesiV/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ToplamaMakinesiV
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBirinciSayi = new System.Windows.Forms.TextBox();
+            this.txtİkinciSayi = new System.Windows.Forms.TextBox();
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Birinci Sayi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "İkinci Sayi";
+            // 
+            // txtBirinciSayi
+            // 
+            this.txtBirinciSayi.Location = new System.Drawing.Point(213, 58);
+            this.txtBirinciSayi.Name = "txtBirinciSayi";
+            this.txtBirinciSayi.Size = new System.Drawing.Size(222, 22);
+            this.txtBirinciSayi.TabIndex = 2;
+            // 
+            // txtİkinciSayi
+            // 
+            this.txtİkinciSayi.Location = new System.Drawing.Point(213, 84);
+            this.txtİkinciSayi.Name = "txtİkinciSayi";
+            this.txtİkinciSayi.Size = new System.Drawing.Size(222, 22);
+            this.txtİkinciSayi.TabIndex = 3;
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.Location = new System.Drawing.Point(249, 124);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(186, 40);
+            this.btnTopla.TabIndex = 4;
+            this.btnTopla.Text = "Topla";
+            this.btnTopla.UseVisualStyleBackColor = true;
+            this.btnTopla.Click += new System.EventHandler(this.BtnTopla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTopla);
+            this.Controls.Add(this.txtİkinciSayi);
+            this.Controls.Add(this.txtBirinciSayi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBirinciSayi;
+        private System.Windows.Forms.TextBox txtİkinciSayi;
+        private System.Windows.Forms.Button btnTopla;
+    }
+}
+
---
+++ ToplamaMakinesiV/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToplamaMakinesiV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTopla_Click(object sender, EventArgs e)
+        {
+            int toplam, birinciSayi, ikinciSayi;
+
+            birinciSayi = Convert.ToInt32(txtBirinciSayi.Text);
+            ikinciSayi = Convert.ToInt32(txtİkinciSayi.Text);
+
+            toplam = birinciSayi + ikinciSayi;
+
+            MessageBox.Show("Toplam sonucu: " + toplam);
+        }
+    }
+}
